3985c0630efcac2d545bcb0d569c828acb4e0ce1
Dimitar Markov <dimitar.markov@sap.com>
2022-09-06 17:24:03 +0300

    Securing the API

--- services/functions/create.js
+++ services/functions/create.js
@@ -3,14 +3,14 @@ import handler from "../util/handler";
 import dynamoDb from "../util/dynamodb";
 
 
-export const maint = handler (async (event) => {
+export const main = handler (async (event) => {
   const data = JSON.parse(event.body);
 
   const params = {
     TableName: process.env.TABLE_NAME,
     Item: {
       // The attributes of the item to be created
-      userId: "123", // The id of the author
+      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId, // The id of the author
       noteId: uuid.v1(), // A unique uuid
       content: data.content, // Parsed from request body
       attachment: data.attachment, // Parsed from request body
