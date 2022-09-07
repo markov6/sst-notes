c0ac6a834dc0e2efc901fcd6a005ec585a3d6a39
Dimitar Markov <dimitar.markov@sap.com>
2022-09-07 16:48:49 +0300

    Enabling COrs

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -2,7 +2,16 @@ import { Table, Bucket } from "@serverless-stack/resources"
 
 export function StorageStack({ stack, app }) {
   //Create an S3 bucket 
-  const bucket = new Bucket(stack, "Uploads");
+  const bucket = new Bucket(stack, "Uploads", {
+    cors: [
+      {
+        max10age: "1 day",
+        allowedOrigins: ["*"],
+        allowedHeaders: ["*"],
+        allowedMethods: ["PUT", "POST", "DELETE", "GET", "HEAD"],
+      },
+    ],
+  });
   //Create the DynamoDB table
   const table = new Table(stack, "Notes", {
     fields: {
