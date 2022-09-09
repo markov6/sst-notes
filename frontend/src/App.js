29c8d89e6cd72cf76a4fd1d8f0a144cd1c0c27bb
Dimitar Markov <dimitar.markov@sap.com>
2022-09-09 17:51:22 +0300

    Adding login/logout functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,30 +3,72 @@ import Nav from "react-bootstrap/Nav";
 import './App.css';
 import Routes from "./Routes";
 import { LinkContainer } from "react-router-bootstrap";
+import React, { useEffect, useState } from "react";
+import { AppContext } from "./lib/contextLib";
+import { Auth } from "aws-amplify";
+import { useNavigate } from "react-router-dom";
+
 
 function App() {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const nav = useNavigate();
+
+  async function handleLogout() {
+    await Auth.signOut();
+    userHasAuthenticated(false);
+    nav("/login");
+  }
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+    
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+     }
+    catch (e) { 
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+    setIsAuthenticating(false);
+  }
+
   return (
-    <div className="App container py-3">
-      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-        <LinkContainer to="/">
-          <Navbar.Brand className="font-weight-bold text-muted">
-            Scratch
-          </Navbar.Brand>
-        </LinkContainer>
+    !isAuthenticating && (
+      <div className="App container py-3">
+        <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+          <LinkContainer to="/">
+            <Navbar.Brand className="font-weight-bold text-muted">
+              Scratch
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
             <Nav activeKey={window.location.pathname}>
-              <LinkContainer to="/signup">
-                <Nav.Link href="/signup">Signup</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link href="/login">Login</Nav.Link>
-              </LinkContainer>
+              {isAuthenticated ? (
+                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+              ) : (
+                <>
+                  <LinkContainer to="/signup">
+                    <Nav.Link>Signup</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <Nav.Link>Login</Nav.Link>
+                  </LinkContainer>
+                </>
+              )}
             </Nav>
           </Navbar.Collapse>
-      </Navbar>
-      <Routes />
-    </div>
+        </Navbar>
+        <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+          <Routes />
+        </AppContext.Provider>
+      </div>
+    )
   );
 }
 
---
+++ frontend/src/containers/Login.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import "./Login.css";
+import { Auth } from "aws-amplify"
+import { useAppContext } from "../lib/contextLib";
+import { useNavigate } from "react-router-dom";
+import LoaderButton from "../components/LoaderButton";
+import { onError } from "../lib/errorLib";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { userHasAuthenticated } = useAppContext();
+  const nav = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  async function handleSubmit(event) { 
+    event.preventDefault();
+    setIsLoading(true);
+    
+    try {
+      await Auth.signIn(email, password);
+      userHasAuthenticated(true);
+      nav("/");
+    } catch (e) { 
+      onError(e);
+    }
+  }
+
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <LoaderButton
+          block="true"
+          size="lg"
+          type="submit"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
+          Login
+        </LoaderButton>
+      </Form>
+    </div>
+  );
+}
