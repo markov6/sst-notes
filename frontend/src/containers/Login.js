13c5ff78207cc93fd70dcf7b6cc1920320cbcb8d
Dimitar Markov <dimitar.markov@sap.com>
2022-09-10 17:01:06 +0300

    Add signup page

--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -1,33 +1,38 @@
 import React, { useState } from "react";
+import { Auth } from "aws-amplify";
 import Form from "react-bootstrap/Form";
-import "./Login.css";
-import { Auth } from "aws-amplify"
-import { useAppContext } from "../lib/contextLib";
 import { useNavigate } from "react-router-dom";
 import LoaderButton from "../components/LoaderButton";
+import { useAppContext } from "../lib/contextLib";
+import { useFormFields } from "../lib/hooksLib";
 import { onError } from "../lib/errorLib";
+import "./Login.css";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const { userHasAuthenticated } = useAppContext();
   const nav = useNavigate();
+  const { userHasAuthenticated } = useAppContext();
   const [isLoading, setIsLoading] = useState(false);
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: "",
+  });
 
   function validateForm() {
-    return email.length > 0 && password.length > 0;
+    return fields.email.length > 0 && fields.password.length > 0;
   }
 
-  async function handleSubmit(event) { 
+  async function handleSubmit(event) {
     event.preventDefault();
+
     setIsLoading(true);
-    
+
     try {
-      await Auth.signIn(email, password);
+      await Auth.signIn(fields.email, fields.password);
       userHasAuthenticated(true);
       nav("/");
-    } catch (e) { 
+    } catch (e) {
       onError(e);
+      setIsLoading(false);
     }
   }
 
@@ -39,16 +44,16 @@ export default function Login() {
           <Form.Control
             autoFocus
             type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={fields.email}
+            onChange={handleFieldChange}
           />
         </Form.Group>
         <Form.Group size="lg" controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={fields.password}
+            onChange={handleFieldChange}
           />
         </Form.Group>
         <LoaderButton
